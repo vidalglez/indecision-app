00ec8563b7c3cc63efbf0e4ec43347f7f092b67f
darkvidal <darkvidal@gmail.com>
2018-04-26 16:11:03 -0400

    Adding textbox and button

--- src/app.js
+++ src/app.js
@@ -3,50 +3,35 @@ console.log('App.js is running!');
 const app = {
     title: 'Blade Runner',
     subtitle: 'The emotion continue',
-    options: ['One', 'Two']
+    options: []
 };
+
+const onFormSubmit = (e) => {
+  e.preventDefault();
+  const option = e.target.elements.option.value;
+  if(option){
+    app.options.push(option);
+    e.target.elements.option.value = ''; 
+  }
+}
 
 const template = (
   <div>
     <h3>{app.title}</h3>
     {app.subtitle && <p>{app.subtitle}</p>}
     <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
+    <p>{app.options.length}</p>
     <ol>
       <ul>Item one</ul>
       <ul>Item two</ul>
     </ol>
+    <form onSubmit={onFormSubmit}>
+      <input type="text" name="option" />
+      <button>Add Option</button>
+    </form>
   </div>
 ); 
 
-const addOne = function(){
-  count++;  
-  renderCounterApp();
-}
-
-const minusOne = () => {
-  count--;
-  renderCounterApp();  
-}
-
-const reset = () => {
-  count = 0;
-  renderCounterApp();  
-}
-
-let count = 0;
 const appRoute = document.getElementById('app');
 
-const renderCounterApp = () => {
-   const template2 = (
-    <div>
-      <h1>Count: {count}</h1>
-      <button id="my-id" onClick={addOne} className="button">+1</button>
-      <button id="minBtn" onClick={minusOne}>-1</button>
-      <button id="resetBtn" onClick={reset}>reset</button>
-    </div>
-  );
-  
-  ReactDOM.render(template2, appRoute);
-};
-
-renderCounterApp();
+ReactDOM.render(template, appRoute);
