d5e9cd0f362ef5c2630e80b35c8a51f3f6b4058a
darkvidal <darkvidal@gmail.com>
2018-04-26 15:23:14 -0400

    Adding Manual Data Binding

--- src/app.js
+++ src/app.js
@@ -18,12 +18,35 @@ const template = (
   </div>
 ); 
 
+const addOne = function(){
+  count++;  
+  renderCounterApp();
+}
+
+const minusOne = () => {
+  count--;
+  renderCounterApp();  
+}
+
+const reset = () => {
+  count = 0;
+  renderCounterApp();  
+}
+
 let count = 0;
-const template2 = (
-  <div>
-    <h1>Count: {count}</h1>
-    <button id="my-id" className="button">+1</button>
-  </div>
-);
 const appRoute = document.getElementById('app');
-ReactDOM.render(template2, appRoute);
+
+const renderCounterApp = () => {
+   const template2 = (
+    <div>
+      <h1>Count: {count}</h1>
+      <button id="my-id" onClick={addOne} className="button">+1</button>
+      <button id="minBtn" onClick={minusOne}>-1</button>
+      <button id="resetBtn" onClick={reset}>reset</button>
+    </div>
+  );
+  
+  ReactDOM.render(template2, appRoute);
+};
+
+renderCounterApp();
