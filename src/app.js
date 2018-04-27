7124cf05e067120e1a7a9158ea0a52534d81d737
darkvidal <darkvidal@gmail.com>
2018-04-27 17:56:44 -0400

    adding text field and add and remove functionality

--- src/app.js
+++ src/app.js
@@ -12,26 +12,41 @@ const onFormSubmit = (e) => {
   if(option){
     app.options.push(option);
     e.target.elements.option.value = ''; 
+    renderTemplate();
   }
 }
 
-const template = (
-  <div>
-    <h3>{app.title}</h3>
-    {app.subtitle && <p>{app.subtitle}</p>}
-    <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
-    <p>{app.options.length}</p>
-    <ol>
-      <ul>Item one</ul>
-      <ul>Item two</ul>
-    </ol>
-    <form onSubmit={onFormSubmit}>
-      <input type="text" name="option" />
-      <button>Add Option</button>
-    </form>
-  </div>
-); 
+const onRemoveAll = () => {
+   // e.preventDefault();
+  app.options = [];
+  renderTemplate();
+}
 
 const appRoute = document.getElementById('app');
 
-ReactDOM.render(template, appRoute);
+const renderTemplate = () => {
+
+  const template = (
+    <div>
+      <h3>{app.title}</h3>
+      {app.subtitle && <p>{app.subtitle}</p>}
+      <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
+      <p>{app.options.length}</p>
+      <button onClick={onRemoveAll}>Remove All</button>
+      <ol>
+        <ul>Item one</ul>
+        <ul>Item two</ul>
+      </ol>      
+      <form onSubmit={onFormSubmit}>
+        <input type="text" name="option" />
+        <button>Add Option</button>
+      </form>
+    </div>
+  ); 
+
+  
+  ReactDOM.render(template, appRoute);
+}
+
+renderTemplate();
+
