1da2bcb4f1443ca7022850ef8341798f4c4a8d21
Vidal Gonzalez <darkvidal@gmail.com>
2018-05-12 11:49:05 -0500

    Adding ES6 import/export

--- src/app.js
+++ src/app.js
@@ -1,1 +1,12 @@
+import {add, square} from './utils.js';
+import {isAdult, canDrink} from './person.js'
+
 console.log('app.js is running!!');
+
+console.log(add(34, 23));
+console.log(square(9));
+
+const age = 17;
+
+console.log(isAdult(age));
+console.log(canDrink(age));
