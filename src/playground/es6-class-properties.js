508e01d5e79e7d269eb910e1f9b8659c39d1efe0
Vidal Gonzalez <darkvidal@gmail.com>
2018-05-15 00:21:22 -0500

    Removing and storing class properties example

---
+++ src/playground/es6-class-properties.js
@@ -0,0 +1,26 @@
+class OldSyntax{
+    constructor(){
+      this.name = 'Mike';
+      this.getGreeting = this.getGreeting.bind(this);
+    }
+  
+    getGreeting(){
+      return `My name is ${this.name}`;
+    }
+  }
+  
+  const oldSyntax = new OldSyntax();
+  const getGreeting = oldSyntax.getGreeting;
+  console.log(getGreeting());
+  
+  class NewSyntax{
+    name = 'Jen';
+  
+    getGreeting = () => {
+      return `My name is ${this.name}`;
+    }
+  }
+  
+  const newSyntax = new NewSyntax();
+  const newGetGreeting = newSyntax.getGreeting; 
+  console.log(newGetGreeting());
