ac9fa6a725a57bd5df5bc408797186c5c144dcd7
Vidal Gonzalez <darkvidal@gmail.com>
2018-05-15 22:14:48 -0500

    Refactoring Other Stateless Functional Components

--- src/components/Option.js
+++ src/components/Option.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const Option = (props) => {
-    return(
-      <div>
-        {props.optionText}
-        <button 
-          onClick={ (e) => {
-            props.handleDeleteOption(props.optionText);
-          }}
-        >
-          Remove
-        </button>
-      </div>
-    );
-  };
+const Option = (props) => (
+  <div>
+    {props.optionText}
+    <button 
+      onClick={ (e) => {
+        props.handleDeleteOption(props.optionText);
+      }}
+    >
+      Remove
+    </button>
+  </div>
+);
 
 export default Option;
