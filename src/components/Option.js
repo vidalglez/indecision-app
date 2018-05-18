ba788ac89cffbc11b7329283f90aa9b9334820e6
darkvidal <darkvidal@gmail.com>
2018-05-18 18:07:10 -0400

    Styling Option Item

--- src/components/Option.js
+++ src/components/Option.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const Option = (props) => (
-  <div>
-    {props.optionText}
+  <div className="option">
+    <p className="option__text">{props.count} {props.optionText}</p>    
     <button
       className="button button--link"
       onClick={(e) => {
